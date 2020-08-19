0435e4b98c2b478e94bcc0274cb8d93beb3d872e
sugarmanwork <54087516+sugarmanDev@users.noreply.github.com>
2020-08-19 16:10:37 +0900

    Update domain.js

--- assets/js/domain.js
+++ assets/js/domain.js
@@ -3,7 +3,7 @@ var getDomain = window.location.hostname;
 var targetDomain = '';
 
 if(getDomain =='salk.co.kr' || getDomain == 'sugarmandev.github.io') {
-  targetDomain = 'http://api.sugarmanwork.com/salk';
+  targetDomain = 'https://gimyo.co.kr/salk';
 }
 else {
   targetDomain = 'http://localhost/salk';
